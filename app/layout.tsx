8a5b042f6c1dd9213ebdf10382b18814df01f28c
MuonYugu <78674802+cpk7778@users.noreply.github.com>
2023-12-08 09:35:29 +0900

    20231208 Work

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({children}:{ children: React.ReactNode }){
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="kr">
+      <body className={`${inter.className} antialiasing`}>{children}</body>
     </html>
   );
 }
