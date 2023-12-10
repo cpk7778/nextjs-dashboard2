65989c6e951e959176fd68b37d9b0205b9e8946d
cpk7778 <cpk7778@gmail.com>
2023-12-10 13:51:31 +0900

    20231210-home

--- app/dashboard/laboratory/page.tsx
+++ app/dashboard/laboratory/page.tsx
@@ -1,3 +1,22 @@
+import {fetchCardData, fetchLatestInvoices, fetchRevenue} from "@/app/lib/data";
+import {Card} from "@/app/ui/dashboard/cards";
+import { lusitana } from '@/app/ui/fonts';
+
 export default function Page() {
-    return <p>Customers Page</p>
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        {' '}
+        Dashboard{' '}
+      </h1>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            <Card title="Collected" value="1" type="collected" />
+            <Card title="Pending" value="1" type="pending" />
+        </div>
+
+        <div className="mt-6 grid grid-cols-1 gap-2 md:grid-cols-4 lg:grid-cols-8">
+            <h1 className="flex">Lab실</h1>
+        </div>
+    </main>
+  );
 }
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {UserGroupIcon, HomeIcon, DocumentDuplicateIcon} from '@heroicons/react/24/outline';
+import {UserGroupIcon, HomeIcon, DocumentDuplicateIcon, ArchiveBoxIcon} from '@heroicons/react/24/outline';
 import Link from "next/link";
 import {usePathname} from "next/navigation";
 import clsx from "clsx";
@@ -8,9 +8,10 @@ import clsx from "clsx";
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  {name: 'Home', href: '/dashboard', icon: HomeIcon},
+  {name: 'Dashboard', href: '/dashboard', icon: HomeIcon},
   {name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon},
   {name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon},
+  {name: 'Laboratory', href: '/dashboard/laboratory', icon: ArchiveBoxIcon},
 ];
 
 export default function NavLinks() {
